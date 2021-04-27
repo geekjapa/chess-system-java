b80d585076e1d0bc071f4ce2ba1e895f284efc74
Adriano Okamori <dri.okamori@gmail.com>
2021-04-27 13:31:14 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,9 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+import boardGame.Board;
+import boardGame.BoardException;
+
+public class ChessException extends BoardException {
 	private static final long serialVersionUID = 1L;
 	
 	public ChessException(String msg) {
